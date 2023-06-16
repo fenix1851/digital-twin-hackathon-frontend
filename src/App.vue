be40a44c9b80c4a7867006fcb5a6a55bba7612f2
Pyatiletov Dmitriy Kirillovich <foxyess@mail.ru>
2023-06-17 00:30:40 +0200

    ant design

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-    <div id="app">
-    worked
+    <div>
+        <a-button type="primary">Primary Button</a-button>
     </div>
 </template>
 
 <script>
+import { Button } from 'ant-design-vue';
 
+export default {
+    components: {
+        'a-button': Button
+    }
+};
 </script>
-
-<style>
-
-</style>
